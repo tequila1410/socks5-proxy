8ad6958b9f6ede9e6edd0fc2c5200c41d57c2a3a
Vladyslav Zubko <vlad.zubko.sk@gmail.com>
2026-08-21 00:14:43 +0200

    refactor: restructure runtime module

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,55 +1,45 @@
+use std::future::Future;
+use std::io::Error;
 use std::sync::Arc;
+use std::time::Duration;
 
 use tokio::join;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Semaphore;
 
 use crate::config::Config;
-use crate::ingress::socks5::handle_socks5_connection;
 use crate::ingress::http_connect::handle_http_connection;
+use crate::ingress::socks5::handle_socks5_connection;
 
 pub async fn run(config: Config) {
     let config = Arc::new(config);
     let semaphore = Arc::new(Semaphore::new(config.connections_limit));
-    join!(accept_http_connect(Arc::clone(&config), Arc::clone(&semaphore)), accept_socks5(Arc::clone(&config), semaphore.clone()));
+    join!(
+        accept_loop(
+            config.http_connect_addr(),
+            Arc::clone(&config),
+            Arc::clone(&semaphore),
+            handle_http_connection,
+        ),
+        accept_loop(
+            config.socks5_addr(),
+            Arc::clone(&config),
+            semaphore,
+            handle_socks5_connection,
+        ),
+    );
 }
 
-async fn accept_http_connect(config: Arc<Config>, semaphore: Arc<Semaphore>) {
-    let listener = TcpListener::bind(config.http_connect_addr()).await.unwrap();
-    loop {
-        match listener.accept().await {
-            Ok((stream, _)) => {
-                println!("Accepted HTTP connection");
-                let config = Arc::clone(&config);
-                let semaphore = Arc::clone(&semaphore);
-                let permit = match semaphore.try_acquire_owned() {
-                    Ok(permit) => permit,
-                    Err(e) => {
-                        println!("Error acquiring permit: {}", e);
-                        continue;
-                    }
-                };
-                tokio::spawn(async move {
-                    let _permit = permit;
-                    match handle_http_connection(stream, config.connect_timeout, config.idle_timeout).await {
-                        Ok(()) => {
-                            
-                        }
-                        Err(e) => {
-                            println!("Error: {}", e);
-                        }
-                    }
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
-}
-
-async fn accept_socks5(config: Arc<Config>, semaphore: Arc<Semaphore>) {
-    let listener = TcpListener::bind(config.socks5_addr()).await.unwrap();
+async fn accept_loop<F, Fut>(
+    addr: String,
+    config: Arc<Config>,
+    semaphore: Arc<Semaphore>,
+    handler: F,
+) where
+    F: Fn(TcpStream, Duration, Duration) -> Fut + Copy + Send + 'static,
+    Fut: Future<Output = Result<(), Error>> + Send,
+{
+    let listener = TcpListener::bind(&addr).await.unwrap();
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
@@ -64,13 +54,9 @@ async fn accept_socks5(config: Arc<Config>, semaphore: Arc<Semaphore>) {
                 };
                 tokio::spawn(async move {
                     let _permit = permit;
-                    match handle_socks5_connection(stream, config.connect_timeout, config.idle_timeout).await {
-                        Ok(()) => {
-                           
-                        }, 
-                        Err(e) => {
-                            println!("Error: {}", e);
-                        }
+                    if let Err(e) = handler(stream, config.connect_timeout, config.idle_timeout).await
+                    {
+                        println!("Error: {}", e);
                     }
                 });
             }
